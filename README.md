5795293a6ac08ddcd13edb25b9f20c3bdb061d39
Chintan Radia <radiachints1512@gmail.com>
2015-10-16 08:24:21 +0000

    init

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # adm-zip-cli
 Extract .zip file from command line
+
+## Usage
+```
+    > unzip --help
+
+      Usage
+        > unzip <filename> <folderpath>
+        
+        <filename>   - file you waant to extract
+        <folderpath> - to which folder you want to extract the files
+    
+      Examples
+        > unzip filename.zip foldername
+        
+```
+
+## Related
+- [adm-zip](https://github.com/cthackers/adm-zip) - API for this module
+
+## License
+
+MIT © [Chintan Radia](http://beatfreaker.github.io/) 
---
+++ cli.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var AdmZip = require('adm-zip');
+var meow = require('meow');
+var cli = meow({
+	help: [
+		'Usage',
+		'  > unzip <filename.zip>',
+		'',
+		'Example',
+		'  > unzip compressed.zip',
+		'    compressed/file1.txt',
+		'    compressed/file2.txt'
+	]
+});
+var zipFile = cli.input[0];
+var dest = cli.input[1];
+if(!zipFile) {
+    console.log('file name is required');
+    process.exit(1);
+}
+if(!dest) {
+    console.log('destination folder name is required');
+    process.exit(1);
+}
+if(zipFile && dest) {
+    var zip = new AdmZip(zipFile);
+    zip.extractAllTo(dest, true);
+}
