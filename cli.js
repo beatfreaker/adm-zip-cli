35fdd05ee399a1053c44b727b396a3590847c54d
Chintan Radia <radiachints1512@gmail.com>
2015-10-16 13:56:31 +0530

    init

--- cli.js
+++ cli.js
@@ -15,14 +15,14 @@ var cli = meow({
 var zipFile = cli.input[0];
 var dest = cli.input[1];
 if(!zipFile) {
-    console.log('file name is required');
-    process.exit(1);
+	console.log('file name is required');
+	process.exit(1);
 }
 if(!dest) {
-    console.log('destination folder name is required');
-    process.exit(1);
+	console.log('destination folder name is required');
+	process.exit(1);
 }
 if(zipFile && dest) {
-    var zip = new AdmZip(zipFile);
-    zip.extractAllTo(dest, true);
+	var zip = new AdmZip(zipFile);
+	zip.extractAllTo(dest, true);
 }
